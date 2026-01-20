ab1b331fddf8d296bef11afad54a5b9db3693393
Cyanoxide <cyanoxide@gmail.com>
2026-01-20 00:55:02 +0000

    Style taskbar window tabs

--- frontend/src/components/TaskBar/TaskBar.tsx
+++ frontend/src/components/TaskBar/TaskBar.tsx
@@ -23,8 +23,18 @@ const TaskBar: React.FC<TaskBarProps> = ({ placeholder = "" }) => {
         <div className={`${styles.taskBar} flex justify-between`}>
             <button className={`${styles.startButton}`}>Start</button>
             <ul className={`${styles.windows} flex items-center justify-start w-full`}>
-                <li>Chrome</li>
-                <li>File Explorer</li>
+                <li data-active="true">
+                    <span className="w-full relative flex">
+                        <img src="/icon__internet_explorer.png" width="14" height="14" className="mr-2 min-w-[14px]"></img>
+                        <span className="absolute ml-7">Internet Explorer</span>
+                    </span>
+                </li>
+                <li>
+                    <span className="w-full relative flex">
+                        <img src="/icon__documents.png" width="14" height="14" className="mr-2 min-w-[14px]"></img>
+                        <span className="absolute ml-7">My Documents</span>
+                    </span>
+                </li>
             </ul>
             <div className={`${styles.systemTray} flex justify-center items-center`}>
                 <span>{formattedTime}</span>
